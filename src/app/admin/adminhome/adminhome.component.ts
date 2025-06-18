a9fbb1f7cc4e383015c0ae940a80bd3be2e578e3
meera37 <meerabenny370@gmail.com>
2025-06-18 22:44:03 +0530

    angular multiselect dropdown installed

--- src/app/admin/adminhome/adminhome.component.ts
+++ src/app/admin/adminhome/adminhome.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AdminsidebarComponent } from "../adminsidebar/adminsidebar.component";
 import { AdminheaderComponent } from "../adminheader/adminheader.component";
+import { ApiService } from '../../service/api.service';
 
 @Component({
   selector: 'app-adminhome',
@@ -9,5 +10,59 @@ import { AdminheaderComponent } from "../adminheader/adminheader.component";
   styleUrl: './adminhome.component.css'
 })
 export class AdminhomeComponent {
+
+  allusers:any =[]
+  allrecipes:any=[]
+  alldownloadsCount:any=0
+
+constructor(private api:ApiService){}
+
+ngOnInit(){
+  this.getcount()
+}
+
+getcount(){
+this.api.getAllUsersApi().subscribe({
+  next:(res:any)=>{
+  //  console.log(res);
+  this.allusers = res.length -1 //or in html allrecipes.length(here all recipes = res)
+  console.log(this.allusers);
+  
+    
+  },
+  error:(err:any)=>{
+   console.log(err);
+  
+    
+  }
+})
+this.api.allRecipesApi().subscribe({
+  next:(res:any)=>{
+   // console.log(res);
+   this.allrecipes = res.length
+    console.log(this.allrecipes);
+    
+  },
+  error:(err:any)=>{
+    console.log(err);
+    
+  }
+})
+this.api.getAllDownloadApi().subscribe({
+  next:(res:any)=>{
+   console.log(res);
+  //  this.alldownloads = res.length
+  //   console.log(this.alldownloads);
+  let count = res.map((item:any)=>item.count).reduce((n1:any, n2:any)=>n1+n2) 
+  this.alldownloadsCount = count 
+  //console.log(count);
+  
+  },
+  error:(err:any)=>{
+    console.log(err);
+    
+  }
+})
+}
 
 }
--- src/app/admin/adminrecipes/adminrecipes.component.ts
+++ src/app/admin/adminrecipes/adminrecipes.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { AdminheaderComponent } from "../adminheader/adminheader.component";
 import { AdminsidebarComponent } from "../adminsidebar/adminsidebar.component";
+import { IDropdownSettings, NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+
 import { ApiService } from '../../service/api.service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { SearchPipe } from '../../pipes/search.pipe';
 
 @Component({
   selector: 'app-adminrecipes',
-  imports: [AdminheaderComponent, AdminsidebarComponent, FormsModule, SearchPipe],
+  imports: [AdminheaderComponent, AdminsidebarComponent, FormsModule, SearchPipe,   NgMultiSelectDropDownModule, ReactiveFormsModule],
   templateUrl: './adminrecipes.component.html',
   styleUrl: './adminrecipes.component.css'
 })
@@ -16,13 +18,63 @@ export class AdminrecipesComponent {
   allRecipes:any = []
   searchKey:string = ""
   cuisineType:any=[]
-mealType:any=[]
+//mealType:any=[]
+dropdownSettings:IDropdownSettings = {};
+dropdownList:any = [];
+ selectedItems:any = [];
 
+recipeForm:FormGroup
+  constructor(private api:ApiService, private fb:FormBuilder){
+    this.recipeForm = fb.group({
+      recipeName:["",[Validators.required, Validators.pattern('[a-zA-Z]*')]],
+      preTime:["",[Validators.required, Validators.pattern('[0-9]*')]],
+      calories:["",[Validators.required, Validators.pattern('[0-9]*')]],
+      serving:["",[Validators.required, Validators.pattern('[0-9]*')]],
+      cookingTime:["",[Validators.required, Validators.pattern('[0-9]*')]],
+      rating:["",[Validators.required, Validators.pattern('[0-9]*')]],
+      modeofCooking:["",[Validators.required, Validators.pattern('[a-zA-Z]*')]],
+      mealType:[[],[Validators.required]],
+      cuisineType:["",[Validators.required]],
+      ingredients:[[],[Validators.required]],
+      instructions:[[],[Validators.required]],
+      image:["",[Validators.required]],
 
-  constructor(private api:ApiService){}
+    })
+
+  }
+
 
   ngOnInit(){
 this.getAllRecipes()
+
+this.dropdownList = [
+      { item_id: 1, item_text: 'Lunch' },
+      { item_id: 2, item_text: 'Dinner' },
+      { item_id: 3, item_text: 'Snack' },
+      { item_id: 4, item_text: 'Dessert' },
+      { item_id: 5, item_text: 'Side Dish' },
+      { item_id: 6, item_text: 'Appetizer' },
+      { item_id: 7, item_text: 'Snacks' },
+      { item_id: 8, item_text: 'Breakfast' },
+      { item_id: 9, item_text: 'Beverage' }
+
+    ];
+
+this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      itemsShowLimit: 3,
+      allowSearchFilter: true
+    };
+  }
+
+  onItemSelect(item: any) {
+    console.log(item);
+  }
+  onSelectAll(items: any) {
+    console.log(items);
   }
 
   getAllRecipes(){
@@ -35,12 +87,12 @@ this.getAllRecipes()
    push(item.cuisine)
 })
 
-this.allRecipes.map((item:any)=>item.mealType).flat().forEach((item:any)=>{
-(!this.mealType.includes(item)) && this.mealType.push(item)
-})
+// this.allRecipes.map((item:any)=>item.mealType).flat().forEach((item:any)=>{
+// (!this.mealType.includes(item)) && this.mealType.push(item)
+// })
   
 console.log(this.cuisineType);
-console.log(this.mealType);
+//console.log(this.mealType);
         
       },
       error:(err)=>{
